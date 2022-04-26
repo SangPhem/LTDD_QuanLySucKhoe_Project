ff0d00c78c94ff27eecb7d22388e2aceff2ab13b
CRKhang <phungkhang13@gmail.com>
2022-04-27 01:11:56 +0700

    NewActi
    NewActi

---
+++ app/src/main/java/com/example/doan/CovidTrackerActivity.java
@@ -0,0 +1,15 @@
+package com.example.doan;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CovidTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_tracker);
+    }
+}
