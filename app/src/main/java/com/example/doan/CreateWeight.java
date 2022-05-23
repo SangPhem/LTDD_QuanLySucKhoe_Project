db090e61dfafbfeb69820ec1df113cfd5e782fad
SangPhem <sangpham170101@gmail.com>
2022-05-23 21:52:17 +0700

    WeightTracker

---
+++ app/src/main/java/com/example/doan/CreateWeight.java
@@ -0,0 +1,124 @@
+package com.example.doan;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import 	android.icu.util.Calendar;
+
+import java.nio.MappedByteBuffer;
+
+public class CreateWeight extends AppCompatActivity {
+
+    EditText mcreateweight, mcreateweightdate;
+    FloatingActionButton msaveweight;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+    DatePickerDialog.OnDateSetListener setListener;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_weight);
+
+        msaveweight = findViewById(R.id.saveweight);
+        mcreateweight = findViewById(R.id.createweight);
+        mcreateweightdate = findViewById(R.id.createweightdate);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        mcreateweightdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateWeight.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month = month + 1;
+                        String date = day+"/"+month+"/"+year;
+                        mcreateweightdate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbarweight);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        msaveweight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String weight = mcreateweight.getText().toString();
+                String date = mcreateweightdate.getText().toString();
+                if(weight.isEmpty() || date.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    DocumentReference documentReference = firebaseFirestore.collection("weights").document(firebaseUser.getUid()).collection("myWeights").document();
+                    Map<String, Object> cannang = new HashMap<>();
+                    cannang.put("weight", weight);
+                    cannang.put("date",date);
+
+                    documentReference.set(cannang).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(getApplicationContext(),"Thêm thành công",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(CreateWeight.this, WeightTrackerActivity.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),"Thêm thất bại",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/doan/WeightTrackerActivity.java
@@ -0,0 +1,31 @@
+package com.example.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class WeightTrackerActivity extends AppCompatActivity {
+
+    FloatingActionButton mcreateweight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_tracker);
+
+        mcreateweight = findViewById(R.id.createweight);
+
+//        getSupportActionBar().setTitle("Bảng lưu cân nặng");
+
+        mcreateweight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WeightTrackerActivity.this, CreateWeight.class));
+            }
+        });
+    }
+}
