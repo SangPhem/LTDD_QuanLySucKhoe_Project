dce7ef42cb6d9eafd321792203bd970609652a5e
CRKhang <phungkhang13@gmail.com>
2022-05-25 13:21:02 +0700

    nav
    nav

--- app/src/main/java/com/example/doan/MainActivity.java
+++ app/src/main/java/com/example/doan/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.doan;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,12 +13,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.common.internal.BaseGmsClient;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
@@ -29,17 +34,19 @@ public class MainActivity extends AppCompatActivity {
     List<Integer> images;
     MainAdapter mainAdapter;
 
-    Button btnLogout;
 
     FirebaseAuth auth;
 
+
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        mDrawerLayout = findViewById(R.id.drawer_layout);
+
         rcvDashboard = findViewById(R.id.rcvDashboard);
-        btnLogout = findViewById(R.id.btnLogout);
 
         auth = FirebaseAuth.getInstance();
 
@@ -55,23 +62,50 @@ public class MainActivity extends AppCompatActivity {
         images.add(R.drawable.stepcounter);
         images.add(R.drawable.heartrate);
         images.add(R.drawable.bmiicon);
+        //
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout_main);
+        findViewById(R.id.image_btn_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                if (id == R.id.nav_profile)
+                {
+                    startActivity(new Intent(MainActivity.this,ProfileFragment.class));
+                }
+                else if (id == R.id.nav_hisactive)
+                {
+                    startActivity(new Intent(MainActivity.this,HisActiveFragment.class));
+
+                }else if (id == R.id.nav_note)
+                {
+                    startActivity(new Intent(MainActivity.this,NoteFragment.class));
+
+                }else if (id == R.id.nav_logout)
+                {
+                    auth.signOut();
+                    finish();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+                return false;
+            }
+        });
+
 
         mainAdapter = new MainAdapter(this, titles, images);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         rcvDashboard.setLayoutManager(gridLayoutManager);
         rcvDashboard.setAdapter(mainAdapter);
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                auth.signOut();
-                finish();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
-
-            }
-        });
 
     }
 }
